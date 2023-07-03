e559df01a388777946956bd2154ddf8ef1960dd2
sangeetha <raok.sangeetha@gmail.com>
2023-07-03 14:54:09 +0530

    add existing files to git

---
+++ src/test/java/com/sangeetha/pagination/paginationDemo/PaginationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangeetha.pagination.paginationDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
